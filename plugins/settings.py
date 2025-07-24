22446dc76c2223c6c752145d1ca90fb4ef72f58b
𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧 😎 <kundanvirajbhatt@gmail.com>
2025-07-24 21:48:27 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -49,7 +49,7 @@ async def send_settings_message(chat_id, user_id):
             Button.inline('❌ Remove Thumbnail', b'remthumb')
         ],
         [
-            Button.url('🆘 Report Errors', 'https://t.me/kundan_yadav_bot)
+            Button.url('🆘 Report Errors', 'https://t.me/team_spy_pro)
         ]
     ]
     await gf.send_message(chat_id, MESS, buttons=buttons)
